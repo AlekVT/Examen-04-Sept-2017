474bc432ac14d3946d6e590d3882a7e02c97dcfc
AlekVT <alek.vtichelen@gmail.com>
2017-09-04 14:42:41 +0200

    added start question 2

--- Bol/src/ap/student/BolClient.java
+++ Bol/src/ap/student/BolClient.java
@@ -9,6 +9,8 @@ public class BolClient {
         	ClientResource resource = new ClientResource("http://127.0.0.1:8182/bol/add");
            	// Post a new trash
         	resource.post(new String[] {"Jefke Vermeulen", "Eikenlei 17", "04/09-17", "Dell Laptopt", "66"});
+        	
+        	resource.post(new String[] {"Hunter S. Thompson", "L.V. Nevada", "12/11/1969", "waterpijp", "666"});
         } catch (Exception e) {
         	System.out.println(e.getMessage());
         }
