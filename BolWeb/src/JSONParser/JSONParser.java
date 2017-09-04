8e50a607c5f2ac8d9cf8ec678c3926c0b3f16232
AlekVT <alek.vtichelen@gmail.com>
2017-09-04 16:19:48 +0200

    finish add method

--- BolWeb/src/JSONParser/JSONParser.java
+++ BolWeb/src/JSONParser/JSONParser.java
@@ -2,9 +2,13 @@ package JSONParser;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonSyntaxException;
 
@@ -23,13 +27,19 @@ public class JSONParser {
 		return products.getProducts();
 	}
 	
-	public List<Product> addProduct(Product product) throws JsonSyntaxException, JsonIOException, FileNotFoundException {
+	public List<Product> addProduct(Product product) throws JsonSyntaxException, JsonIOException, IOException {
 		List<Product> exist = this.getProducts();
+		//add product to list
 		exist.add(product);
+		//writes list to json file
+		Writer w = new FileWriter(INPUTFILE);
+		Gson json = new GsonBuilder().create();
+		json.toJson(exist, w);
+		
 		return this.getProducts();
 	}
 	
-	public List<String[]> findProducts(String query) {
+	public List<Product> findProducts(String query) {
 		return null;
 	}
 }
